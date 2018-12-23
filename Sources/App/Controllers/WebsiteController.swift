dca8eb5d1bfc6a3890eb3c2e792f9e93cd240863
Afiq Hamdan <afiq@Afiqs-MacBook-Pro.local>
2018-12-24 04:19:50 +0800

    Add leaf and created WebsiteController

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -12,12 +12,36 @@ struct WebsiteController: RouteCollection {
     
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("acronyms", Acronym.parameter, use: acronymHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
-        let context = IndexContent(title: "Meow 😺")
-        return try req.leaf().render("index", context)
+        return Acronym.query(on: req).all().flatMap(to: View.self) { acronyms in
+            let context = IndexContent(title: "HomePage", acronyms: acronyms.isEmpty ? nil : acronyms)
+            for acronym in acronyms {
+                print(acronym.short)
+            }
+            return try req.leaf().render("index", context)
+        }
     }
+    
+    func acronymHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Acronym.self).flatMap(to: View.self) { acronym in
+            return acronym.creator.get(on: req).flatMap(to: View.self) { creator in
+                let context = AcronymContext(title: acronym.long, acronym: acronym, creator: creator)
+                return try req.leaf().render("acronym", context)
+            }
+        }
+    }
+    
+//    func acronymHandler(_ req: Request) throws -> Future<View> {
+//        return try req.parameters.next(Acronym.self).flatMap(to: View.self) { acronym in
+//            return acronym.creator.get(on: req).flatMap(to: View.self) { creator in
+//                let context = AcronymContext(title: acronym.long, acronym: acronym, creator: creator)
+//                return try req.leaf().render("acronym", context)
+//            }
+//        }
+//    }
     
 }
 
@@ -29,4 +53,11 @@ extension Request {
 
 struct IndexContent: Encodable {
     let title: String
+    let acronyms: [Acronym]?
+}
+
+struct AcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let creator: User
 }
