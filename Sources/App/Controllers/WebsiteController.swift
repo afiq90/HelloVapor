b829cae06408a1fdeecba190ff35543b910e7d68
Afiq Hamdan <afiq@Afiqs-MacBook-Pro.local>
2018-12-24 04:53:55 +0800

    Add users page

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -13,6 +13,8 @@ struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
         router.get("acronyms", Acronym.parameter, use: acronymHandler)
+        router.get("users", User.parameter, use: userHandler)
+
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -34,15 +36,14 @@ struct WebsiteController: RouteCollection {
         }
     }
     
-//    func acronymHandler(_ req: Request) throws -> Future<View> {
-//        return try req.parameters.next(Acronym.self).flatMap(to: View.self) { acronym in
-//            return acronym.creator.get(on: req).flatMap(to: View.self) { creator in
-//                let context = AcronymContext(title: acronym.long, acronym: acronym, creator: creator)
-//                return try req.leaf().render("acronym", context)
-//            }
-//        }
-//    }
-    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(User.self).flatMap(to: View.self) { user in
+            return try user.acronyms.query(on: req).all().flatMap(to: View.self) { acronyms in
+                let context = UserContext(title: user.name, user: user, acronyms: acronyms.isEmpty ? nil : acronyms)
+                return try req.leaf().render("user", context)
+            }
+        }
+    }
 }
 
 extension Request {
@@ -60,4 +61,10 @@ struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let creator: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]?
 }
